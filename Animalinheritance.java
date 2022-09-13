c673cd438329b26390c830823564982c7ef0a84b
willyklindt <you@example.com>
2022-09-13 11:25:22 +0200

    Jeg har lavet hund nr. 5

--- Animalinheritance.java
+++ Animalinheritance.java
@@ -1,16 +1,18 @@
 public class Animalinheritance {
 
     public static void main(String[] args) {
-        Animal a1, a2, a3, a4;
+        Animal a1, a2, a3, a4, a5;
         a1 = new Animal();
         a2 = new Dog();
         a3 = new Doggy();
         a4 = new Doggy();
+        a5 = new Doggy();
 
         a1.sound();
         a2.sound();
         a3.sound();
         a4.sound();
+        a5.sound();
     }
 }
 
