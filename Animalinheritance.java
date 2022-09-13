1a6e19edd6a6d71056f35400b51f30067204673b
Jacob Peter Diesel Nielsen <jpn@ruc.dk>
2022-09-13 10:56:52 +0200

    Jeg har lavet en ændring

--- Animalinheritance.java
+++ Animalinheritance.java
@@ -1,14 +1,16 @@
 public class Animalinheritance {
 
     public static void main(String[] args) {
-        Animal a1, a2, a3;
+        Animal a1, a2, a3, a4;
         a1 = new Animal();
         a2 = new Dog();
         a3 = new Doggy();
+        a4 = new Doggy();
 
         a1.sound();
         a2.sound();
         a3.sound();
+        a4.sound();
     }
 }
 
