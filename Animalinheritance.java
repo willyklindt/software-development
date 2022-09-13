1923327c42710db377e22c6d4ffc3f489b84f3cc
TechDiesel <jpn@ruc.dk>
2022-09-13 11:39:25 +0200

    Update Animalinheritance.java

--- Animalinheritance.java
+++ Animalinheritance.java
@@ -1,11 +1,19 @@
 public class Animalinheritance {
 
     public static void main(String[] args) {
+<<<<<<< HEAD
         Animal a1, a2, a3, a4;
+=======
+        Animal a1, a2, a3, a4, a5;
+>>>>>>> parent of 8df7f6f (Der er nu en dog a6)
         a1 = new Animal();
         a2 = new Dog();
         a3 = new Doggy();
         a4 = new Doggy();
+<<<<<<< HEAD
+=======
+        a5 = new Doggy();
+>>>>>>> parent of 8df7f6f (Der er nu en dog a6)
 
         a1.sound();
         a2.sound();
