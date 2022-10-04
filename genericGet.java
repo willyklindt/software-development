5f867a2c0d4cb014cf589181ca6f1ee2f23e95b9
willyklindt <wgk@ruc.dk>
2022-10-04 11:37:46 +0200

    committed files

---
+++ genericGet.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class genericGet <T> {
+
+public static void main(String[] args) {
+    genericGet myFind = new genericGet<>();
+    ArrayList<String> myStringList = new ArrayList<>(Arrays.asList("We", "love", "chips"));
+    System.out.println(myFind.getItem(myStringList, 0));
+}
+
+
+   public genericGet(){}; 
+
+   public T getItem (ArrayList<T> list, Integer index) {
+        if (index >= 0 && index < list.size()){
+            return list.get(index);
+        }
+    return null;
+
+   }
+   
+}
